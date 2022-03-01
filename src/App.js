3d1c3319b597d1b7c6a50d567efca7e40ce4b8a6
subrat1729 <subratpatnaik.email@gmail.com>
2022-03-01 20:49:35 +0530

    Updated API calls and basic front end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import News from "./Components/News";
+import Navbar from "./Components/Navbar";
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
-function App() {
+const App = () => {
+  const [countryCode, setCountryCode] = useState("in");
+  const genres = [
+    "general",
+    "business",
+    "entertainment",
+    "health",
+    "science",
+    "sports",
+    "technology",
+  ];
+  const countryCodes = [
+    "ae",
+    "ar",
+    "at",
+    "au",
+    "be",
+    "bg",
+    "br",
+    "ca",
+    "ch",
+    "cn",
+    "co",
+    "cu",
+    "cz",
+    "de",
+    "eg",
+    "fr",
+    "gb",
+    "gr",
+    "hk",
+    "hu",
+    "id",
+    "ie",
+    "il",
+    "in",
+    "it",
+    "jp",
+    "kr",
+    "lt",
+    "lv",
+    "ma",
+    "mx",
+    "my",
+    "ng",
+    "nl",
+    "no",
+    "nz",
+    "ph",
+    "pl",
+    "pt",
+    "ro",
+    "rs",
+    "ru",
+    "sa",
+    "se",
+    "sg",
+    "si",
+    "sk",
+    "th",
+    "tr",
+    "tw",
+    "ua",
+    "us",
+    "ve",
+    "za",
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar
+          updateCountryCode={setCountryCode}
+          genres={genres}
+          countryCodes={countryCodes}
+          curCountry={countryCode}
+        />
+        <Routes>
+          <Route exact path="/" element={<Navigate to="/general" />} />
+          {genres.map((genre) => (
+            <Route
+              exact
+              path={`/${genre}`}
+              element={<News countryCode={countryCode} genre={genre} />}
+            />
+          ))}
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
